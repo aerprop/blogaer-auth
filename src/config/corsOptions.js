24418615fc0c83ccf483c0ab2fcc2804c1d740f2
Andika Eka Putra <andika.eka.102@gmail.com>
2023-08-24 21:02:39 +0700

    feat: Implement CORS for cross-origin resource sharing

---
+++ src/config/corsOptions.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionSuccessStatus: 200,
+  credentials: true
+};
+
+export default corsOptions;
