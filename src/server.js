89e1e1fd2117895e0c7f33b51b9f9dc739294b19
Andika Eka Putra <andika.eka.102@gmail.com>
2023-08-24 23:08:21 +0700

    feat: Implement cookie-parser for handling HTTP cookies

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import corsOptions from './config/corsOptions';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(cors(corsOptions));
+app.use(cookieParser());
