aa4c055ae7f7d6d8e7e88beec1cec3b728002a23
Andika Eka Putra <andika.eka.102@gmail.com>
2023-08-28 20:01:26 +0700

    refactor: Rename migration and seeders files

---
+++ src/migrations/20230817185734-create-user.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'users',
+      {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4
+        },
+        username: {
+          allowNull: false,
+          unique: true,
+          type: Sequelize.STRING
+        },
+        email: {
+          allowNull: false,
+          unique: true,
+          type: Sequelize.STRING
+        },
+        password: {
+          allowNull: false,
+          type: Sequelize.STRING
+        },
+        picture: {
+          type: Sequelize.STRING
+        },
+        role_id: {
+          allowNull: false,
+          type: Sequelize.TINYINT,
+          defaultValue: 2,
+          references: {
+            model: 'user_roles',
+            key: 'id'
+          }
+        },
+        verified: {
+          type: Sequelize.BOOLEAN
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updated_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        deleted_at: {
+          type: Sequelize.DATE,
+          defaultValue: null
+        }
+      });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
