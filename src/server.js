8b09ac0025ee6f889957ae6678e6648b82b80fb5
Andika Eka Putra <andika.eka.102@gmail.com>
2023-09-16 08:26:38 +0700

    refactor: remove dotenv, add verifyToken middleware, add blogpost route, rename root route to base route

--- src/server.js
+++ src/server.js
@@ -3,9 +3,8 @@ import cors from 'cors';
 import corsOptions from './config/corsOptions.js';
 import cookieParser from 'cookie-parser';
 import routes from './routes/index.js';
-import { config } from 'dotenv';
+import verifyToken from './middlewares/verifyToken.js';
 
-config();
 const app = express();
 const PORT = process.env.PORT || 3939;
 
@@ -13,11 +12,14 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(routes.root);
+app.use(routes.base);
 app.use(routes.register);
 app.use(routes.login);
 app.use(routes.logout);
 app.use(routes.refresh);
+
+app.use(verifyToken);
+app.use(routes.blogPost);
 
 app.listen(PORT, () =>
   console.log(`Server running on port: ${PORT}`)
