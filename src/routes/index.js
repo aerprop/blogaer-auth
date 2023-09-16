a4c57bf1b11dded582b200ed589b83508db25c38
Andika Eka Putra <andika.eka.102@gmail.com>
2023-09-16 08:29:37 +0700

    feat: rename root to base, add blogpost route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,17 @@
-import rootRoute from './rootRoute.js';
+import baseRoute from './baseRoute.js';
 import registerRoute from './registerRoute.js';
 import loginRoute from './loginRoute.js';
 import logoutRoute from './logoutRoute.js';
 import refreshTokenRoute from './refreshTokenRoute.js';
+import blogPostProxyRoute from './blogPostProxyRoute.js';
 
 const routes = {};
 
-routes.root = rootRoute;
+routes.base = baseRoute;
 routes.register = registerRoute;
 routes.login = loginRoute;
 routes.logout = logoutRoute;
 routes.refresh = refreshTokenRoute;
+routes.blogPost = blogPostProxyRoute;
 
 export default routes;
