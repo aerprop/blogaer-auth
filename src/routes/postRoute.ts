6ccc1b247436fe0ce2162f3c050ae3bb52f59c0a
anekra <andika.eka.102@gmail.com>
2024-09-22 13:21:09 +0700

    Feature: adding channel middleware.

--- src/routes/postRoute.ts
+++ src/routes/postRoute.ts
@@ -1,14 +1,28 @@
 import { Router } from 'express';
 import verifyAuthor from '../middlewares/verifyAuthor';
 import postController from '../controllers/postController';
-import rabbitMQConnection from '../middlewares/rabbitMQ';
+import postTopicChan from '../middlewares/postTopicChan';
+import postRpcChan from '../middlewares/postRpcChan';
 
 export default Router()
-  .use([verifyAuthor, rabbitMQConnection])
-  .post(`${process.env.BASE_ROUTE}/post`, postController.addPost)
-  .get(`${process.env.BASE_ROUTE}/post`, postController.getPostsByQuery)
-  .get(`${process.env.BASE_ROUTE}/post/:postId`, postController.getPostById)
-  .get(`${process.env.BASE_ROUTE}/post/explore`, postController.explorePosts)
-  .put(`${process.env.BASE_ROUTE}/post/:postId`, postController.updatePost)
-  .patch(`${process.env.BASE_ROUTE}/post/:postId`, postController.patchPost)
-  .delete(`${process.env.BASE_ROUTE}/post/:postId`, postController.deletePost);
+  .use(verifyAuthor)
+  .get(`${process.env.BASE_ROUTE}/post/user`, [
+    postRpcChan,
+    postController.getPostsByUserId
+  ])
+  .post(`${process.env.BASE_ROUTE}/post`, [
+    postTopicChan,
+    postController.addPost
+  ])
+  .patch(`${process.env.BASE_ROUTE}/post/:slug`, [
+    postTopicChan,
+    postController.patchPost
+  ])
+  .put(`${process.env.BASE_ROUTE}/post/:slug`, [
+    postTopicChan,
+    postController.updatePost
+  ])
+  .delete(`${process.env.BASE_ROUTE}/post/:slug`, [
+    postTopicChan,
+    postController.deletePost
+  ]);
