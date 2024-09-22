6ce4432b8e50c9809f8ff801790708f7f6f36112
anekra <andika.eka.102@gmail.com>
2024-09-22 13:15:54 +0700

    Refactor: change the way on how to connect to mysql.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,72 +1,74 @@
 'use strict';
-
-import DataTypes, { Sequelize } from 'sequelize';
-import process from 'process';
+import { DataTypes, Dialect, Sequelize } from 'sequelize';
 import sequelizeConfig from '../config/sequelizeConfig';
 import User from './user';
 import UserRole from './userRole';
 import RefreshToken from './refreshToken';
 import UserProvider from './userProvider';
 
-interface SequelizeConfig {
-  development: {
-    username: string | undefined;
-    password: string | undefined;
-    database: string | undefined;
-    host: string | undefined;
-    dialect: string;
-    use_env_variable?: string;
-  };
-}
-
-type Models = {
-  User: ReturnType<typeof User>;
-  UserRole: ReturnType<typeof UserRole>;
-  RefreshToken: ReturnType<typeof RefreshToken>;
-  UserProvider: ReturnType<typeof UserProvider>;
+export type Models = {
+  user: ReturnType<typeof User>;
+  userRole: ReturnType<typeof UserRole>;
+  refreshToken: ReturnType<typeof RefreshToken>;
+  userProvider: ReturnType<typeof UserProvider>;
   dataTypes: typeof DataTypes;
-  Sequelize: Sequelize;
+  sequelize: Sequelize;
 };
 
-let sequelizeObj: Sequelize;
-const config: SequelizeConfig['development'] = sequelizeConfig.development;
-if (config.use_env_variable) {
-  sequelizeObj = new Sequelize(
-    process.env[config.use_env_variable] as string,
-    config as any
-  );
-} else {
-  sequelizeObj = new Sequelize(
-    config.database as string,
-    config.username as string,
-    config.password as string,
-    config as any
-  );
+async function mysqlConnect(sequelize: Sequelize, retries = 0) {
+  try {
+    const connection = await sequelize.authenticate();
+    console.log('Connected to mysql ✔✔✔');
+
+    return connection;
+  } catch (error) {
+    console.log(
+      'Failed to connect to MySQL:',
+      retries < 5
+        ? 'Retrying in 60 seconds.'
+        : 'Max retries have been reached.'
+    );
+    if (retries >= 5) {
+      throw new Error('Failed to connect to MySQL after 5 attempts ✖✖✖');
+    }
+    await new Promise((resolve) => setTimeout(resolve, 60000));
+
+    return mysqlConnect(sequelize, retries + 1);
+  }
 }
 
-sequelizeObj
-  .authenticate()
-  .then(() => {
-    sequelizeObj.sync();
-    console.log('Connected to mysql ✔✔✔');
-  })
-  .catch((error: Error) => {
-    console.error('Unable to connect to the database', error);
+async function init() {
+  const config = sequelizeConfig.development;
+  const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+      dialect: config.dialect as Dialect,
+      host: config.host,
+      logging: false
+    }
+  );
+
+  mysqlConnect(sequelize)
+    .then(() => sequelize.sync())
+    .catch((err) => console.log('Sequelize sync failed ✖✖✖', err));
+
+  const models: Models = {
+    user: User(sequelize, DataTypes),
+    userRole: UserRole(sequelize, DataTypes),
+    refreshToken: RefreshToken(sequelize, DataTypes),
+    userProvider: UserProvider(sequelize, DataTypes),
+    dataTypes: DataTypes,
+    sequelize: sequelize
+  };
+  Object.values(models).forEach((model) => {
+    if ('associate' in model) {
+      model.associate(models);
+    }
   });
 
-const models: Models = {
-  User: User(sequelizeObj, DataTypes),
-  UserRole: UserRole(sequelizeObj, DataTypes),
-  RefreshToken: RefreshToken(sequelizeObj, DataTypes),
-  UserProvider: UserProvider(sequelizeObj, DataTypes),
-  dataTypes: DataTypes,
-  Sequelize: sequelizeObj
-};
+  return models;
+}
 
-Object.values(models).forEach((model) => {
-  if ('associate' in model) {
-    model.associate(models);
-  }
-});
-
-export default models;
+export default init();
