61fff4924517ce85ec19f2aa515e7dcd11ca294d
anekra <andika.eka.102@gmail.com>
2024-09-22 13:25:52 +0700

    Feature: adding when to delete refresh token mechanism in the server.

--- src/models/refreshToken.ts
+++ src/models/refreshToken.ts
@@ -1,21 +1,22 @@
 'use strict';
-
-import sequelize, { Model, Sequelize } from 'sequelize';
-import Models from '.';
+import type { Models } from '.';
+import { DataTypes, Sequelize, Model } from 'sequelize';
 
 interface RefreshTokenModel {
   token: string;
   userId: string;
+  clientId: string;
+  createdAt?: typeof DataTypes.DATE;
+  updatedAt?: typeof DataTypes.DATE;
 }
 
 interface RefreshToken extends Model<RefreshTokenModel>, RefreshTokenModel {}
 
 export type RefreshTokenStatic = typeof Model & {
   new (values?: Record<string, unknown>, options?: any): RefreshToken;
-  associate: (models: typeof Models) => void;
+  associate: (models: Models) => void;
 };
 
-const DataTypes = sequelize;
 const RefreshToken = (sequelize: Sequelize, dataTypes: typeof DataTypes) => {
   const refreshToken = sequelize.define<RefreshToken>(
     'RefreshToken',
@@ -27,17 +28,37 @@ const RefreshToken = (sequelize: Sequelize, dataTypes: typeof DataTypes) => {
       userId: {
         allowNull: false,
         type: dataTypes.UUID
+      },
+      clientId: {
+        allowNull: false,
+        type: dataTypes.STRING
+      },
+      createdAt: {
+        type: dataTypes.DATE
+      },
+      updatedAt: {
+        type: dataTypes.DATE
       }
     },
     {
       tableName: 'refresh_tokens',
-      underscored: true
+      underscored: true,
+      hooks: {
+        async beforeUpdate(instance, _) {
+          if (!instance.updatedAt) return;
+          const updatedAt = new Date(instance.updatedAt.toString()).getTime();
+          const sessionTimeLimit = new Date(updatedAt + 24 * 60 * 60 * 1000).getTime();
+          const now = Date.now();
+
+          if (sessionTimeLimit < now) await instance.destroy();
+        }
+      }
     }
   ) as RefreshTokenStatic;
 
-  refreshToken.associate = (models: typeof Models) => {
-    if (models.User) {
-      refreshToken.belongsTo(models.User, {
+  refreshToken.associate = (models: Models) => {
+    if (models.user) {
+      refreshToken.belongsTo(models.user, {
         foreignKey: 'user_id',
         targetKey: 'id'
       });
