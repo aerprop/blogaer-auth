492fe92ebe773bbdcb32c691f8b271673a9bf72e
anekra <andika.eka.102@gmail.com>
2025-04-20 10:18:01 +0700

    Feature: updating getSecurity to also include user requests and add addOrResetPassword function.

--- src/controllers/user/security/securityController.ts
+++ src/controllers/user/security/securityController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
-import mainModel from '../../../models/MainModel';
 import userService from '../../../services/user/userService';
 import User from '../../../models/User';
 import MainModel from '../../../models/MainModel';
-import { Op } from 'sequelize';
+import { CommonStatus, EmailSubject } from '../../../utils/enums';
+import userRequestService from '../../../services/user/userRequestService';
+import bcrypt from 'bcrypt';
+import { generateClientId } from '../../../utils/helper';
 
 type UserJoins =
   | (User & {
@@ -17,15 +19,20 @@ type UserJoins =
 const securityController = {
   async getSecurity(req: Request, res: Response) {
     const { userId } = req;
-    const model = await mainModel;
+    const model = await MainModel;
     if (!model) {
-      console.log('Database connection failed!');
+      console.log('securityController.ts >>> Database connection failed!');
       return res.status(500).json({
         status: 'Internal server error',
         error: 'Database connection failed!'
       });
     }
-    const userOauth = await userService.getOauthAssociations(userId);
+
+    const refreshToken = req.cookies[`${process.env.REFRESH_TOKEN}`];
+    const token = await model.refreshToken.findByPk(refreshToken, {
+      attributes: ['clientId']
+    });
+
     const userJoins = (await model.user.findByPk(userId, {
       attributes: ['password'],
       include: [
@@ -44,25 +51,83 @@ const securityController = {
       ]
     })) as UserJoins;
 
+    const userPasswordExist = userJoins?.password ? true : null;
     const userPasskeys = userJoins?.UserPasskeys;
     const isUserPasskeys = userPasskeys ? userPasskeys.length > 0 : false;
     const isTwoFAEnabled = userJoins?.UserSetting?.twoFaEnabled;
     const twoFAMethod = userJoins?.UserSetting?.twoFaMethod;
     const userSecret = userJoins?.UserTotpSecret;
+    const userOauth = await userService.getOauthAssociations(userId);
+
+    const emailSubject = userPasswordExist
+      ? EmailSubject.ResetPassword
+      : EmailSubject.AddPassword;
+    const foundUserRequest = await userRequestService.getUserRequest(
+      model,
+      userId,
+      emailSubject,
+      token?.clientId
+    );
+    const userRequest =
+      foundUserRequest !== null
+        ? {
+            request: foundUserRequest?.request,
+            limit: foundUserRequest?.limit.getTime(),
+            status: foundUserRequest?.status
+          }
+        : null;
 
     return res.status(200).json({
       status: 'Success',
       data: {
-        userPassword: userJoins?.password ? true : null,
+        userPassword: userPasswordExist,
         userTwoFA: {
           twoFAMethod: twoFAMethod != null ? twoFAMethod : null,
           isTwoFAEnabled: isTwoFAEnabled != null ? isTwoFAEnabled : null,
           isPasskey: isUserPasskeys ? true : null,
           isAuthApp: userSecret ? true : null
         },
-        userOauth
+        userOauth,
+        userRequest
       }
     });
+  },
+  async addOrResetPassword(req: Request, res: Response) {
+    const { password, subject, limit } = req.body;
+    const { userId } = req;
+    const model = await MainModel;
+    if (!model) {
+      console.log('Database connection failed!');
+      return res.status(500).json({
+        status: 'Internal server error',
+        error: 'Database connection failed!'
+      });
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const [updatedData] = await model.user.update(
+      { password: hashPassword },
+      {
+        where: { id: userId }
+      }
+    );
+
+    const { clientId } = generateClientId(req.headers['user-agent']);
+    if (!clientId) {
+      return res
+        .status(400)
+        .json({ status: 'Bad request', error: 'User agent is invalid!' });
+    }
+    if (updatedData === 1) {
+      await model.userRequest.update(
+        { status: CommonStatus.Success },
+        { where: { clientId, request: subject, limit } }
+      );
+      const status = subject === EmailSubject.AddPassword ? 201 : 200;
+
+      return res.sendStatus(status);
+    } else {
+      return res.sendStatus(204);
+    }
   }
 };
 
