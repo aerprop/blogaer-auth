b8cb17f17adffa6410f5c166d9de928f7f2ff994
anekra <andika.eka.102@gmail.com>
2025-04-20 09:28:05 +0700

    Add new enums.

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -2,3 +2,24 @@ export enum TwoFAMethod {
   Passkey = 'passkey',
   App = 'app'
 }
+
+export enum EmailSubject {
+  AddPassword = 'add-password',
+  ResetPassword = 'reset-password',
+  UpdateEmail = 'update-email',
+  UpdateUsername = 'update-username'
+}
+
+export enum CommonStatus {
+  Success = 'success',
+  Rejected = 'rejected',
+  Expired = 'expired',
+  Pending = 'pending'
+}
+
+export enum OauthProvider {
+  Apple = 'Apple',
+  Google = 'Google',
+  Github = 'Github',
+  Microsoft = 'Microsoft'
+}
