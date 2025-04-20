3624e5efcaee4aa1f0208647407655ddc2c0bb39
anekra <andika.eka.102@gmail.com>
2025-04-20 10:32:28 +0700

    Feature: adding get account, patch email, and patch username routes.

--- src/routes/protected/user/accountRoute.ts
+++ src/routes/protected/user/accountRoute.ts
@@ -1,9 +1,21 @@
-import accountController from '../../../controllers/user-settings/accountController';
+import accountController from '../../../controllers/user/accountController';
+import verifyRequestForm from '../../../middlewares/verifyRequestForm';
+import verifyRequestFormOtp from '../../../middlewares/verifyRequestFormOtp';
 import { router } from '../../router';
 
-const accountRoute = router().patch(
-  `${process.env.BASE_ROUTE}/user/account`,
-  accountController.patchAccount
-);
+const accountRoute = router()
+  .get(`${process.env.BASE_ROUTE}/user/account`, accountController.getAccount)
+  .patch(
+    `${process.env.BASE_ROUTE}/user/account`,
+    accountController.patchAccount
+  )
+  .patch(`${process.env.BASE_ROUTE}/user/account/update-email`, [
+    verifyRequestFormOtp,
+    accountController.patchAccount
+  ])
+  .patch(`${process.env.BASE_ROUTE}/user/account/update-username`, [
+    verifyRequestForm,
+    accountController.patchAccount
+  ]);
 
 export default accountRoute;
